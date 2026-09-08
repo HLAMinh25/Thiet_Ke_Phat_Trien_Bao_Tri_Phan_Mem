a3ee2204132aca28f373963a26a2bfb78c49ad3c
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-08 20:46:23 +0700

    Add files via upload

--- Tuan01_Greational Design Pattern/DPM235449_HuynhLuAnhMinh_Tuan01_Singleton_Real_NhanVien_DP/Program.cs
+++ Tuan01_Greational Design Pattern/DPM235449_HuynhLuAnhMinh_Tuan01_Singleton_Real_NhanVien_DP/Program.cs
@@ -2,48 +2,6 @@
 
 namespace Bai5_Singleton
 {
-    public sealed class UserSessionManager
-    {
-        private static UserSessionManager _instance;
-        private static readonly object _lock = new object();
-
-        public string LoggedInUserId { get; private set; }
-        public string StaffName { get; private set; }
-        public string Role { get; private set; }
-        public DateTime LoginTime { get; private set; }
-
-        private UserSessionManager() { }
-
-        public static UserSessionManager GetInstance()
-        {
-            if (_instance == null)
-            {
-                lock (_lock)
-                {
-                    if (_instance == null)
-                    {
-                        _instance = new UserSessionManager();
-                    }
-                }
-            }
-            return _instance;
-        }
-
-        public void Login(string userId, string staffName, string role)
-        {
-            LoggedInUserId = userId;
-            StaffName = staffName;
-            Role = role;
-            LoginTime = DateTime.Now;
-            Console.WriteLine($"[Đăng nhập thành công] NV: {StaffName} | Quyền: {Role} | Thời gian: {LoginTime:HH:mm:ss}");
-        }
-
-        public void DisplaySession()
-        {
-            Console.WriteLine($"[Session Hiện Tại] UserID: {LoggedInUserId} | Name: {StaffName} | Role: {Role}");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -62,7 +20,7 @@ namespace Bai5_Singleton
             // So sánh 2 thể hiện
             if (ReferenceEquals(session1, session2))
             {
-                Console.WriteLine("\n[Xác nhận] Chi có DUY NHẤT 1 thể hiện SessionManager tồn tại trong bộ nhớ.");
+                Console.WriteLine("\n[Xác nhận] Chỉ có DUY NHẤT 1 thể hiện SessionManager tồn tại trong bộ nhớ.");
             }
 
             Console.ReadLine();
