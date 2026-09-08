b5816c0275347a1fc8d2c0454131c06e7278cc7f
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-08 20:40:35 +0700

    Add files via upload

---
+++ Tuan01_Greational Design Pattern/DPM235449_HuynhLuAnhMinh_Tuan01_Facade_Real_NhanVien_DP/FinancialSubsystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NongDuocAnGiang.DesignPatterns.Facade
+{
+    public class FinancialSubsystem
+    {
+        public string CalculateTotalRevenue(DateTime fromDate, DateTime toDate)
+        {
+            return $"[Tài chính] Tổng doanh thu bán hàng: 150,000,000 VND";
+        }
+
+        public string SummarizeDiscountsAndShipping()
+        {
+            return "[Tài chính] Chi phí vận chuyển: 2,500,000 VND | Tổng giảm giá: 1,200,000 VND";
+        }
+    }
+}
---
+++ Tuan01_Greational Design Pattern/DPM235449_HuynhLuAnhMinh_Tuan01_Facade_Real_NhanVien_DP/InventorySubsystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NongDuocAnGiang.DesignPatterns.Facade
+{
+    public class InventorySubsystem
+    {
+        public string GetInventoryData(DateTime fromDate, DateTime toDate)
+        {
+            return $"[Kho] Dữ liệu tồn kho lô hàng từ {fromDate:dd/MM/yyyy} đến {toDate:dd/MM/yyyy}";
+        }
+
+        public string CheckLowStockItems()
+        {
+            return "[Kho] Cảnh báo: Thuốc trừ sâu sinh học A1 sắp hết hạn/hết hàng";
+        }
+    }
+}
