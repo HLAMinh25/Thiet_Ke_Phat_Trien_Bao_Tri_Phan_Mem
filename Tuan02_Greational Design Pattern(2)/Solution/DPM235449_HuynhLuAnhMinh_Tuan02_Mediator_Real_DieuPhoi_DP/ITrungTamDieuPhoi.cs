f040060339071879a4b39713b66798078185525a
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 15:58:40 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Mediator_Real_DieuPhoi_DP/ITrungTamDieuPhoi.cs
@@ -0,0 +1,8 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Mediator_Real_DieuPhoi_DP
+{
+    // Interface Trung tâm điều phối hệ thống bán hàng nông dược An Giang
+    public interface ITrungTamDieuPhoi
+    {
+        void ThongBao(object nguonPhat, string suKien, object? duLieu = null);
+    }
+}
