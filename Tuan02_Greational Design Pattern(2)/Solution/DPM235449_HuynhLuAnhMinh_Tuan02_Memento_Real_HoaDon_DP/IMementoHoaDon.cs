3983bd479bcf76f62d0392ee5c809a5b393a5a85
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 15:59:31 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Memento_Real_HoaDon_DP/IMementoHoaDon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DPM235407_NguyenTuanAnh_Tuan02_Memento_Real_HoaDon_DP
+{
+    public interface IMementoHoaDon
+    {
+        DateTime GetThoiGian();
+        string GetMoTa();
+    }
+}
---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Observer_DP/IObserver.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+{
+    public interface IObserver
+    {
+        // Receive update from subject
+        void Update(ISubject subject);
+    }
+}
