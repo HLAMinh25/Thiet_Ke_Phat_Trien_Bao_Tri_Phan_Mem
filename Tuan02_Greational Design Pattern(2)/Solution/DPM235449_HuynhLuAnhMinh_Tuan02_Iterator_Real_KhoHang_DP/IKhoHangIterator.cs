46693bdcd2df69ca156e6c7dd33f0d82e5ab3c01
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 15:57:51 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Iterator_Real_KhoHang_DP/IKhoHangIterator.cs
@@ -0,0 +1,11 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Iterator_Real_KhoHang_DP
+{
+    // Interface Iterator duyệt lô hàng
+    public interface IKhoHangIterator
+    {
+        bool HasNext();
+        LoHangNongDuoc? Next();
+        LoHangNongDuoc? CurrentItem();
+        void Reset();
+    }
+}
