cd279a4e91a236dcf48ef4f821ad84ff931c56a1
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:01:22 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Strategy_DP/ConcreteStrategyB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace RefactoringGuru.DesignPatterns.Strategy.Conceptual
+{
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+}
