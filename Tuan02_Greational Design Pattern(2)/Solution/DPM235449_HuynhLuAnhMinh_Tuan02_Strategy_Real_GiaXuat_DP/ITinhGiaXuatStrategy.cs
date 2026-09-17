44b11fedab56fe97a06512a842177f3ac5f77643
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:02:13 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Strategy_Real_GiaXuat_DP/ITinhGiaXuatStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DPM235407_NguyenTuanAnh_Tuan02_Strategy_Real_GiaXuat_DP
+{
+    // Strategy Interface: Định nghĩa thuật toán tính giá xuất sản phẩm
+    public interface ITinhGiaXuatStrategy
+    {
+        string TenPhuongPhap { get; }
+        KetQuaTinhGiaXuat TinhGiaXuat(List<LoThuocNhap> danhSachLo, int soLuongCanXuat);
+    }
+}
---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Template Method_DP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.TemplateMethod.Conceptual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Same client code can work with different subclasses:");
+
+            Client.ClientCode(new ConcreteClass1());
+
+            Console.Write("\n");
+
+            Console.WriteLine("Same client code can work with different subclasses:");
+            Client.ClientCode(new ConcreteClass2());
+        }
+    }
+}
