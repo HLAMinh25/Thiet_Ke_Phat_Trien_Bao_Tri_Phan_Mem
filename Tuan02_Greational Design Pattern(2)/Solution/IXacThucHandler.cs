be0923035861f06029b27e65db5ccde1b7d04bb1
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:04:32 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/IXacThucHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_ChainOfResponsibility_Real_XacThuc_DP
+{
+    // Interface khai báo bước xử lý xác thực
+    public interface IXacThucHandler
+    {
+        IXacThucHandler SetNext(IXacThucHandler handler);
+        bool XuLy(YeuCauTruyCap yeuCau);
+    }
+}
