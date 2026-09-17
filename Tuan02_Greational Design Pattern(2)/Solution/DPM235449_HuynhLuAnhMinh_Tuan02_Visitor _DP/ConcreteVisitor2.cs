ab429f62285cc178fae5e02fcc3a224e6f69a2b2
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:03:00 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Visitor _DP/ConcreteVisitor2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.Visitor.Conceptual
+{
+    class ConcreteVisitor2 : IVisitor
+    {
+        public void VisitConcreteComponentA(ConcreteComponentA element)
+        {
+            Console.WriteLine(element.ExclusiveMethodOfConcreteComponentA() + " + ConcreteVisitor2");
+        }
+
+        public void VisitConcreteComponentB(ConcreteComponentB element)
+        {
+            Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor2");
+        }
+    }
+}
