c20e431346e05e5d5af73b1f36bf93e711ac3057
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 15:56:53 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Command_Real_HoaDon_DP/IHoaDonCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Command_Real_HoaDon_DP
+{
+    // Interface lệnh điều khiển thao tác trên hóa đơn
+    public interface IHoaDonCommand
+    {
+        void Execute();
+        void Undo();
+        string GetMoTa();
+    }
+}
