504c5865a53e49a0943d0007aa3118052310f0cc
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 15:55:55 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Command_DP/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringGuru.DesignPatterns.Command.Conceptual
+{
+    // The Command interface declares a method for executing a command.
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Command_DP/SimpleCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace RefactoringGuru.DesignPatterns.Command.Conceptual
+{
+    // Some commands can implement simple operations on their own.
+    class SimpleCommand : ICommand
+    {
+        private string _payload = string.Empty;
+
+        public SimpleCommand(string payload)
+        {
+            this._payload = payload;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({this._payload})");
+        }
+    }
+}
