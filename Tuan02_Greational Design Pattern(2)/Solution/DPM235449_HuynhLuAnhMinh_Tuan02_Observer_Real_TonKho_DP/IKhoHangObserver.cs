76b95579d4e5bae209f3194b0ad256243a02fb2e
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:00:20 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Observer_Real_TonKho_DP/IKhoHangObserver.cs
@@ -0,0 +1,8 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Observer_Real_TonKho_DP
+{
+    public interface IKhoHangObserver
+    {
+        void CapNhat(ThongTinCanhBaoKho canhBao);
+        string TenBoPhan { get; }
+    }
+}
