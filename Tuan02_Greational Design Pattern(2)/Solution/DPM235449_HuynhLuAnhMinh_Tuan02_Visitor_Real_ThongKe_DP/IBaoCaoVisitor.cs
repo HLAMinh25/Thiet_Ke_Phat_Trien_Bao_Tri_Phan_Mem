ffc7673e0735d04cbdfdb56e77d037432ab7314d
Huỳnh Lư Anh Minh <minhhuynhanh.2k5@gmail.com>
2026-09-17 16:03:45 +0700

    Add files via upload

---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Visitor_Real_ThongKe_DP/IBaoCaoVisitor.cs
@@ -0,0 +1,11 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Visitor_Real_ThongKe_DP
+{
+    // Visitor Interface
+    public interface IBaoCaoVisitor
+    {
+        void VisitHoaDon(HoaDonBanHangElement hoaDon);
+        void VisitLoHangTonKho(LoHangTonKhoElement loHang);
+        void VisitDichVuPhatSinh(DichVuPhatSinhElement dichVu);
+        void InKetQuaBaoCao();
+    }
+}
---
+++ Tuan02_Greational Design Pattern(2)/Solution/DPM235449_HuynhLuAnhMinh_Tuan02_Visitor_Real_ThongKe_DP/IDoiTuongDuLieuElement.cs
@@ -0,0 +1,8 @@
+﻿namespace DPM235407_NguyenTuanAnh_Tuan02_Visitor_Real_ThongKe_DP
+{
+    // Interface Element nhận Visitor
+    public interface IDoiTuongDuLieuElement
+    {
+        void Accept(IBaoCaoVisitor visitor);
+    }
+}
